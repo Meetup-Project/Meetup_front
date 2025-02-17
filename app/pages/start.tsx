af1ccbc20047548575a323794a43965b177c3d0b
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-02-17 23:45:37 +0900

    font 폴더 추가

--- app/pages/start.tsx
+++ app/pages/start.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function start() {
   return (
     <>
-    <div id="background" className=" w-full flex flex-col relative items-center bg-white">
+    <div id="background" className=" w-full flex flex-col items-center bg-white">
       <div id="logodiv" className=' w-full flex h-auto ml-[3vw]'>
         <img id='logo' className=" w-[14vw] h-[7vh]" src='./logo.svg'></img>
       </div>
@@ -14,9 +14,9 @@ function start() {
         <span id='introducename' className=" text-[10vw] font-extrabold">밋업</span>
       </div>
 
-      <div id='characterDiv' className=' bg-graygray w-[20vw] h-[20vh] z-10'></div>
-      <div id='SignInDiv'></div>
-      <div id='SignUpDiv'></div>
+      <div id='characterDiv' className=' bg-graygray w-[50vw] h-[30vh] z-10 overflow-visible mt-[8vw]'></div>
+      <div id='SignInDiv' className='bg-blueblue flex justify-center items-center w-[30vw] h-[5vh] rounded-[15px]'></div>
+      <div id='SignUpDiv' className=''></div>
     </div>
     </>
   )
