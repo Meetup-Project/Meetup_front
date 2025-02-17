e6fd2769d325a4a2eea59535a06c75627d969cf0
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-02-17 18:40:21 +0900

    introduce  멘트 작성

--- app/pages/start.tsx
+++ app/pages/start.tsx
@@ -8,10 +8,12 @@ function start() {
       <div id="logodiv" className=' w-full flex h-auto ml-[3vw]'>
         <img id='logo' className=" w-[14vw] h-[7vh]" src='./logo.svg'></img>
       </div>
-      <div id='introduceDiv' className=' flex flex-col items-center text-blueblue'>
-        <span id='introducecontent'>당신의 일정을 만들어보세요</span>
-        <span id='introducename'>밋업</span>
+
+      <div id='introduceDiv' className=' flex flex-col items-center text-blueblue mt-[10vw]'>
+        <span id='introducecontent' className=' text-[6vw] font-extrabold'>당신의 일정을 만들어보세요</span>
+        <span id='introducename' className="">밋업</span>
       </div>
+
       <div id='characterDiv'></div>
       <div id='SignInDiv'></div>
       <div id='SignUpDiv'></div>
