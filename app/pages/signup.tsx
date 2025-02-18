1a28657509ad9dcbab18f3df26be57d5dd1b6dce
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-02-18 21:33:56 +0900

    start 완성

--- app/pages/signup.tsx
+++ app/pages/signup.tsx
@@ -2,7 +2,16 @@ import React from 'react'
 
 function signup() {
   return (
-    <div>signup</div>
+    <div id='background'>
+        <div id='topDiv'>
+            <span id='signupText'>회원가입</span>
+            <div id='backbutton'></div>
+        </div>
+        <input id='name'></input>
+        <input id='password'></input>
+        <input id='passwordcheck'></input>
+        <input id='email'></input>
+    </div>
   )
 }
 
