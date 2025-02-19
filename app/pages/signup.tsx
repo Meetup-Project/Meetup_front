2bbd03bec5996669b3d8803947add4b63343a0de
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-02-20 00:41:56 +0900

    router back 만들기

--- app/pages/signup.tsx
+++ app/pages/signup.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import { useRouter } from 'next/navigation'
+
 
 function signup() {
+  const router = useRouter()
+
+  function goBack() {
+    router.back();
+  }
   return (
     <div id='background' className=' bg-white flex flex-col items-center p-[10vw]'>
         <div id='topDiv' className=' flex items-center justify-between w-full'>
             <span id='signupText' className='font-pretendard font-extrabold text-[6vw]'>회원가입</span>
-            <div id='backbutton' className=' rounded-[8px] bg-bluegray w-[9vw] h-[4.7vh] flex justify-center items-center'></div>
+            <div id='backbutton' onClick={goBack} className=' rounded-[8px] bg-bluegray w-[9vw] h-[4.7vh] flex justify-center items-center'></div>
         </div>
         <div id='contentDiv' className='flex flex-col gap-[10vh] mt-[10vh] w-full items-center'>
           <input id='name' placeholder='이름' type='string'></input>
