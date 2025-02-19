ac9ce4bb5507dc389095a6b18811587afdd614bc
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-02-20 00:59:04 +0900

    svg 추가 및 input 스타일 작성 완료

--- app/pages/signup.tsx
+++ app/pages/signup.tsx
@@ -12,13 +12,15 @@ function signup() {
     <div id='background' className=' bg-white flex flex-col items-center p-[10vw]'>
         <div id='topDiv' className=' flex items-center justify-between w-full'>
             <span id='signupText' className='font-pretendard font-extrabold text-[6vw]'>회원가입</span>
-            <div id='backbutton' onClick={goBack} className=' rounded-[8px] bg-bluegray w-[9vw] h-[4.7vh] flex justify-center items-center'></div>
+            <div id='backbutton' onClick={goBack} className=' rounded-[8px] bg-bluegray w-[9vw] h-[4.7vh] flex justify-center items-center'>
+              <img id="backimg" src="/arrow-right-sm.svg" alt="arrow-right" />
+            </div>
         </div>
-        <div id='contentDiv' className='flex flex-col gap-[10vh] mt-[10vh] w-full items-center'>
-          <input id='name' placeholder='이름' type='string'></input>
-          <input id='password' placeholder='비밀번호' type='password'></input>
-          <input id='passwordcheck' placeholder="비밀번호 확인" type='password'></input>
-          <input id='email' placeholder='이메일' type='email'></input>
+        <div id='contentDiv' className='flex flex-col gap-[5vh] mt-[10vh] w-full items-center'>
+          <input id='name' placeholder='이름' type='string' className=' bg-graygray w-[80vw] h-[8vh] rounded-[12px] p-[5vw] placeholder: font-bold font-pretendard'></input>
+          <input id='password' placeholder='비밀번호' type='password' className=' bg-graygray w-[80vw] h-[8vh] rounded-[12px] p-[5vw] placeholder: font-bold font-pretendard'></input>
+          <input id='passwordcheck' placeholder="비밀번호 확인" type='password' className=' bg-graygray w-[80vw] h-[8vh] rounded-[12px] p-[5vw] placeholder: font-bold font-pretendard'></input>
+          <input id='email' placeholder='이메일' type='email' className=' bg-graygray w-[80vw] h-[8vh] rounded-[12px] p-[5vw] placeholder: font-bold font-pretendard'></input>
         </div>
         <div id='bottomDiv' className="">
           <div id='subbmit button'></div>
