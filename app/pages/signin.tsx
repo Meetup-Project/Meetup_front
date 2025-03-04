0926dba098ba65fb26040e979de30ab0746dd487
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-03-04 12:29:01 +0900

    일단 주서처리중

--- app/pages/signin.tsx
+++ app/pages/signin.tsx
@@ -26,55 +26,51 @@ function SignIn() {
   
       console.log('로그인 성공:', response.data);
       
-      // JWT 토큰을 로컬 스토리지에 저장
       if (response.data.token) {
         localStorage.setItem('token', response.data.token);
       }
   
-      router.push('/SStart'); // 로그인 성공 후 메인 페이지로 이동
+      router.push('/SStart');
     } catch (error) {
       console.error('로그인 실패:', error);
       setErrorMessage('❌ 로그인 정보가 올바르지 않습니다.');
     }
   };
+  
   return (
-    <div id='background' className='bg-white flex flex-col items-center p-[10vw]'>
-      <div id='topDiv' className='flex items-center justify-between w-full'>
-        <span id='signupText' className='font-pretendard font-extrabold text-[6vw]'>로그인</span>
-        <div id='backbutton' onClick={goBack} className='rounded-[8px] bg-bluegray w-[9vw] h-[4.7vh] flex justify-center items-center'>
-          <img id='backimg' src='/arrow-right-sm.svg' alt='arr  ow-right' />
-        </div>
+    <div className='w-full max-w-[430px] min-w-[320px] min-h-screen flex flex-col items-center bg-white mx-auto px-4'>
+      {/* 맨위 탑바 */}
+      <div className='flex items-center justify-between w-full py-4'>
+        <span className=' text-black font-pretendard font-extrabold text-3xl md:text-2xl'>로그인</span>
+        <button onClick={goBack} className='rounded-lg bg-bluegray w-10 h-10 flex justify-center items-center'>
+          <img src='/arrow-right-sm.svg' alt='뒤로가기' />
+        </button>
       </div>
 
-      <div id='contentDiv' className='flex flex-col gap-[5vh] mt-[4vh] w-full items-center'>
-        <input id='email' value={EmailValue} onChange={(e) => SetEmailValue(e.target.value)} placeholder='이메일' type='text'
-          className='bg-graygray w-[80vw] h-[8vh] rounded-[12px] p-[5vw] placeholder:font-bold font-pretendard'/>
-        <input id='password' value={PasswordValue} onChange={(e) => SetPasswordValue(e.target.value)} placeholder='비밀번호' type='password'
-          className='bg-graygray w-[80vw] h-[8vh] rounded-[12px] p-[5vw] placeholder:font-bold font-pretendard'/>
+      {/*메인 입력칸  */}
+      <div className='flex flex-col gap-6 mt-8 w-full items-center'>
+        <input value={EmailValue} onChange={(e) => SetEmailValue(e.target.value)} placeholder='이메일' type='text'
+          className='bg-gray-200 w-full max-w-md h-12 rounded-lg px-4 placeholder:font-bold' />
+        <input value={PasswordValue} onChange={(e) => SetPasswordValue(e.target.value)} placeholder='비밀번호' type='password'
+          className='bg-gray-200 w-full max-w-md h-12 rounded-lg px-4 placeholder:font-bold' />
         {ErrorMessage && <p className='text-red-500 font-bold'>{ErrorMessage}</p>}
       </div>
 
-      <div id='divideline' className='flex items-center gap-2 w-full mt-[10vh]'>
+      <div className='flex items-center gap-2 w-full max-w-md mt-10'>
         <div className='flex-1 border-t border-gray-300'></div>
         <span className='text-gray-400 font-semibold text-sm'>or</span>
         <div className='flex-1 border-t border-gray-300'></div>
       </div>
 
-      <div id='OauthDiv' className='w-[60vw] flex flex-col gap-[4vh] mt-[5vh]'>
-        <button id='google' className='border-2 border-googlebord rounded-[9999px] w-full h-[6vh]'>
-          Google 로그인
-        </button>
-        <button id='kakao' className='w-full h-[6vh] rounded-[12px] bg-kakaobg'>
-          카카오 로그인
-        </button>
-        <button id='naver' className='w-full h-[6vh] rounded-[4px] bg-naverbg'>
-          네이버 로그인
-        </button>W
+      <div className='w-full max-w-md flex flex-col gap-4 mt-8'>
+        <button className='border-2 border-gray-400 rounded-full w-full h-12 text-gray-700 font-semibold'>Google 로그인</button>
+        <button className='w-full h-12 rounded-lg bg-yellow-400 text-black font-semibold'>카카오 로그인</button>
+        <button className='w-full h-12 rounded-md bg-green-500 text-white font-semibold'>네이버 로그인</button>
       </div>
 
-      <div id='bottomDiv' className='flex flex-row-reverse w-full mt-[10vh]'>
-        <button onClick={handleLogin} className='rounded-[8px] bg-blueblue w-[12vw] h-[6vh] flex justify-center items-center'>
-          <img id='submit arrow' src='/arrow-left-sm.svg' alt='로그인' />
+      <div className='flex justify-end w-full max-w-md'>
+        <button onClick={handleLogin} className='rounded-lg bg-blue-500 w-12 h-12 flex justify-center items-center'>
+          <img src='/arrow-left-sm.svg' alt='로그인' />
         </button>
       </div>
     </div>
