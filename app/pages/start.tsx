59ad8bb79474d49556006d2251235a76d16e66b4
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-03-04 11:08:54 +0900

    start 페이지 반응형 설정

--- app/pages/start.tsx
+++ app/pages/start.tsx
@@ -2,39 +2,44 @@
 import React from 'react'
 import { useRouter } from 'next/navigation'
 
-
-function start() {
+function Start() {
   const router = useRouter();
 
-  function goToSignIn(){
+  function goToSignIn() {
     router.push('./SignIn');
   }
 
   function goToSignUP() {
     router.push('./SignUp');
   }
+  
   return (
-    <>
-    <div id="background" className=" w-full flex flex-col items-center bg-white">
-      <div id="logodiv" className=' w-full flex h-auto ml-[3vw]'>
-        <img id='logo' className=" w-[14vw] h-[7vh]" src='./logo.svg'></img>
+    <div className="w-full max-w-[430px] min-w-[320px] h-screen flex flex-col items-center bg-white mx-auto px-4 sm:px-6 md:px-8">
+      {/* 로고 */}
+      <div className='w-full flex h-auto ml-[3vw] sm:ml-[2vw] md:ml-0'>
+        <img className="w-[14vw] h-[7vh] sm:w-[12vw] md:w-[10vw]" src='./logo.svg' alt="logo" />
       </div>
 
-      <div id='introduceDiv' className=' flex flex-col items-center text-blueblue mt-[8vw] gap-[10vw]'>
-        <span id='introducecontent' className=' text-[6vw] font-extrabold font-pretendard'>당신의 일정을 만들어보세요</span>
-        <span id='introducename' className=" text-[10vw] font-extrabold font-pretendard">밋업</span>
+      {/* 소개 문구 */}
+      <div className='flex flex-col items-center text-blueblue mt-[8vw] gap-[10vw] sm:gap-[8vw] md:gap-[6vw]'>
+        <span className='text-[6vw] sm:text-[5vw] md:text-[4vw] font-extrabold font-pretendard'>당신의 일정을 만들어보세요</span>
+        <span className="text-[10vw] sm:text-[8vw] md:text-[6vw] font-extrabold font-pretendard">밋업</span>
       </div>
 
-      <div id='characterDiv' className=' bg-graygray w-[70vw] h-[40vh] z-10 overflow-visible mt-[8vw]'></div>
-      <div id='SignInDiv' className='bg-blueblue flex justify-center items-center w-[50vw] h-[6vh] rounded-[15px] mt-[5vh]' onClick={goToSignIn}>
-        <span id='signinText' className=' font-pretendard text-white text-[5vw] font-bold'>로그인</span>
+      {/* 캐릭터 이미지 영역 */}
+      <div className='bg-graygray w-[70vw] h-[40vh] sm:w-[60vw] sm:h-[35vh] md:w-[50vw] md:h-[30vh] z-10 overflow-visible mt-[8vw]'></div>
+      
+      {/* 로그인 버튼 */}
+      <div className='bg-blueblue flex justify-center items-center w-[50vw] h-[6vh] sm:w-[45vw] sm:h-[5.5vh] md:w-[40vw] md:h-[5vh] rounded-[15px] mt-[5vh] cursor-pointer' onClick={goToSignIn}>
+        <span className='font-pretendard text-white text-[5vw] sm:text-[4vw] md:text-[3vw] font-bold'>로그인</span>
       </div>
-      <div id='SignUpDiv' className=' border-2 border-blueblue flex justify-center items-center w-[50vw] h-[6vh] rounded-[15px] mt-[5vh]' onClick={goToSignUP}>
-        <span id='signupText' className=' font-pretendard text-blueblue text-[5vw] font-bold'>회원가입</span>
+      
+      {/* 회원가입 버튼 */}
+      <div className='border-2 border-blueblue flex justify-center items-center w-[50vw] h-[6vh] sm:w-[45vw] sm:h-[5.5vh] md:w-[40vw] md:h-[5vh] rounded-[15px] mt-[5vh] cursor-pointer' onClick={goToSignUP}>
+        <span className='font-pretendard text-blueblue text-[5vw] sm:text-[4vw] md:text-[3vw] font-bold'>회원가입</span>
       </div>
     </div>
-    </>
-  )
+  );
 }
 
-export default start
+export default Start;
