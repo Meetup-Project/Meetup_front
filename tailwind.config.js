24c3d6f5366cfc3975b4011570fe38c227b62d74
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-03-18 17:06:32 +0900

    글자 크기 커스텀 스타일

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'blueblue': '#4B6BF5',
+        'graygray': '#EFEFEF',
+      },
+      fontSize: {
+        'sm': ['0.875rem', '1.25rem'],
+        'base': ['1rem', '1.5rem'],
+        'lg': ['1.25rem', '1.75rem'],
+        'xl': ['1.75rem', '2.25rem'],
+      },
+    },
+  },
+  plugins: [],
+}
